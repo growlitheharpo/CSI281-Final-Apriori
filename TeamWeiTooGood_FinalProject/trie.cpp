c1c6d714d565d2a41d4a310b2595d5f5822b5b20
James Keats <growlitheharpo@gmail.com>
2015-12-08 21:57:56 -0500

    Finalize the public functions needed for apriori. Get your shit together David

--- TeamWeiTooGood_FinalProject/trie.cpp
+++ TeamWeiTooGood_FinalProject/trie.cpp
@@ -1,28 +1,58 @@
 #include "trie.h"
 #include "simpleQueue.h"
 
+
+/*********************************************************************************************
+*  Purpose:	Default constructor
+*      Pre:	None
+*	  Post:	Trie is initialized
+*********************************************************************************************/
 Trie::Trie()
 {
 	mRootNode = new Node();
 	mRootNode->mItemId = -1;    //No itemId
 }
 
+
+/*********************************************************************************************
+*  Purpose:	Destructor
+*      Pre:	None
+*	  Post:	Trie is destroyed and memory is released.
+*********************************************************************************************/
 Trie::~Trie()
 {
 	destroySubtrie(mRootNode);
 }
 
-void Trie::setMinSupport(DynamicArray<int> path, bool hasMinSupport)
+
+/*********************************************************************************************
+*  Purpose:	Set minimum support for the node at a given path
+*      Pre:	Handed the path to the node, and whether or not it has support
+*	  Post:	Node is updated
+*********************************************************************************************/
+void Trie::setMinSupport(const DynamicArray<int> &path, bool hasMinSupport)
 {
 
 }
 
-bool Trie::getHasMinSupport(DynamicArray<int> path) const
+
+/*********************************************************************************************
+*  Purpose:	Determine if the node at a given node meets minimum support.
+*      Pre:	Handed the path
+*	  Post:	Returns true if the node at the given path has min support, false otherwise.
+*********************************************************************************************/
+bool Trie::getHasMinSupport(const DynamicArray<int> & path) const
 {
 
 }
 
-bool Trie::addNode(DynamicArray<int> path, int itemId)
+
+/*********************************************************************************************
+*  Purpose:	Add a node to the tree with the given itemId and path
+*      Pre:	Handed the path and the id of the new item
+*	  Post:	Item is added to the tree
+*********************************************************************************************/
+bool Trie::addNode(const DynamicArray<int> &path, int itemId)
 {
 	if (path.count() == 0)
 	{
@@ -43,6 +73,12 @@ bool Trie::addNode(DynamicArray<int> path, int itemId)
 	}
 }
 
+
+/*********************************************************************************************
+*  Purpose:	Destroy a subtrie in the trie.
+*      Pre:	Handed the node at which to begin deleting
+*	  Post:	Subtrie is deleted
+*********************************************************************************************/
 void Trie::destroySubtrie(Node *node)
 {
 	if (isLeaf(node))
@@ -60,11 +96,19 @@ void Trie::destroySubtrie(Node *node)
 	}
 }
 
+
+/*********************************************************************************************
+*  Purpose:	Get ALL potential paths (itemsets) stored in the tree
+*      Pre:	Handed the declared DA<DA<int>> where the paths will be stored.
+*	  Post:	All paths stored in the trie are inserted into the array.
+*********************************************************************************************/
 void Trie::getAllPaths(DynamicArray<DynamicArray<int>> &allPaths) const
 {
 	SimpleQueue<Node *> nodeQueue;
 	Node *currentNode;
 	nodeQueue.enqueue(mRootNode);
+
+	allPaths.clear();
 
 	while (nodeQueue.getCount() > 0)
 	{
@@ -78,23 +122,46 @@ void Trie::getAllPaths(DynamicArray<DynamicArray<int>> &allPaths) const
 	}
 }
 
-void Trie::getAllPathsAtDepth(DynamicArray<DynamicArray<int>>& pathsAtDepth, int depth) const
+
+/*********************************************************************************************
+*  Purpose:	Get all paths from the nodes at a certain depth/level of the tree.
+*      Pre:	Handed the DA<DA<int>> to store the paths, and what depth/level to get them from.
+*	  Post:	pathsAtDepth is filled with the paths at the given depth/level.
+*********************************************************************************************/
+void Trie::getAllPathsAtDepth(DynamicArray<DynamicArray<int>> &pathsAtDepth, int depth) const
 {
 
 }
 
 
+/*********************************************************************************************
+*  Purpose:	Is the trie empty?
+*      Pre:	None
+*	  Post:	True if root has no children, false otherwise
+*********************************************************************************************/
 bool Trie::isEmpty() const
 {
 	return mRootNode->mChildren.count() == 0;
 }
 
+
+/*********************************************************************************************
+*  Purpose:	Determine if a given node is a leaf.
+*      Pre:	Handed a node.
+*	  Post:	True if node is a leaf, false otherwise.
+*********************************************************************************************/
 bool Trie::isLeaf(Node *node) const
 {
 	return node->mChildren.count() == 0;
 }
 
-bool Trie::removeNode(DynamicArray<int> path)
+
+/*********************************************************************************************
+*  Purpose:	Remove a node at a given path.
+*      Pre:	Handed the path to remove the node from.
+*	  Post:	Returns false if the item could not be found, otherwise true and item is removed.
+*********************************************************************************************/
+bool Trie::removeNode(const DynamicArray<int> &path)
 {
 	//   int lastNode = path[path.count() - 1];
 	//   path.removeAt(path.count() - 1);
@@ -104,7 +171,13 @@ bool Trie::removeNode(DynamicArray<int> path)
 	return false;
 }
 
-Trie::Node* Trie::traverseTrie(DynamicArray<int> path, Node *node)
+
+/*********************************************************************************************
+*  Purpose:	david what the fuck is this
+*      Pre:	
+*	  Post:	
+*********************************************************************************************/
+Trie::Node* Trie::traverseTrie(const DynamicArray<int> &path, Node *node) const
 {
 	for (int i = 0; i < path.count(); i++)
 	{
--- TeamWeiTooGood_FinalProject/trie.h
+++ TeamWeiTooGood_FinalProject/trie.h
@@ -37,21 +37,21 @@ class Trie
 
 		void destroySubtrie(Node *node);
 		bool isLeaf(Node *node) const;
-		Node* traverseTrie(DynamicArray<int> path, Node *someNode);
+		Node* traverseTrie(const DynamicArray<int> &path, Node *someNode) const;
 
 	public:
 		Trie();
 		~Trie();
 
-		void setMinSupport(DynamicArray<int> path, bool hasMinSupport);
-		bool getHasMinSupport(DynamicArray<int> path) const;
+		void setMinSupport(const DynamicArray<int> &path, bool hasMinSupport);
+		bool getHasMinSupport(const DynamicArray<int> &path) const;
 
 		void getAllPaths(DynamicArray<DynamicArray<int>> &allPaths) const;
 		void getAllPathsAtDepth(DynamicArray<DynamicArray<int>> &pathsAtDepth, int depth) const;
 
-		bool addNode(DynamicArray<int> path, int itemId);
+		bool addNode(const DynamicArray<int> &path, int itemId);
 		bool isEmpty() const;
-		bool removeNode(DynamicArray<int> path);
+		bool removeNode(const DynamicArray<int> &path);
 
 };
 
