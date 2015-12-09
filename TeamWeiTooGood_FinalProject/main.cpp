20ea2f965f7f14c7a044f48f3f7a3cec9b98594b
David Horntvedt <dbhorntvedt@gmail.com>
2015-12-08 23:18:58 -0500

    Debug stuff

--- TeamWeiTooGood_FinalProject/main.cpp
+++ TeamWeiTooGood_FinalProject/main.cpp
@@ -5,71 +5,89 @@
 //Name this main once we're ready to start
 int actualMain()
 {
-	TimerSystem timer;
-	Trie largeItemsets;
-	ArrayInfo2D transactionSizeInfo;
-	bool **transactions = NULL;
+  TimerSystem timer;
+  Trie largeItemsets;
+  ArrayInfo2D transactionSizeInfo;
+  bool **transactions = NULL;
 
-	string filename = "";
-	bool keepRunning = true;
-	int minimumSupport = 0;
-	double time;
+  string filename = "";
+  bool keepRunning = true;
+  int minimumSupport = 0;
+  double time;
 
-	while (keepRunning)
-	{
-		filename = getFileName();
+  while (keepRunning)
+  {
+    filename = getFileName();
 
-		if (!loadData(transactions, transactionSizeInfo, filename))
-			continue;
+    if (!loadData(transactions, transactionSizeInfo, filename))
+      continue;
 
-		minimumSupport = getMinimumSupport(transactionSizeInfo.sizeJ);
+    minimumSupport = getMinimumSupport(transactionSizeInfo.sizeJ);
 
-		timer.startClock();
-		runApriori(const_cast<const bool**>(transactions), transactionSizeInfo, minimumSupport, largeItemsets);
-		time = timer.getTime();
+    timer.startClock();
+    runApriori(const_cast<const bool**>(transactions), transactionSizeInfo, minimumSupport, largeItemsets);
+    time = timer.getTime();
 
-		outputResults(time, largeItemsets);
+    outputResults(time, largeItemsets);
 
-		keepRunning = getContinue();
-	}
+    keepRunning = getContinue();
+  }
 
-	return 0;
+  return 0;
 }
 
 int main()
 {
-	//bool keepRunning = getContinue();
-	//cout << "Keep running is " << (keepRunning ? "true" : "false") << ".\n";
-
-	/*
-	Trie trie;
-	DynamicArray<int> path;
-
-	trie.addNode(path, 5);*/
-
-	/*
-	DynamicArray<int> array1, array2, output;
-
-	array1.insert(2);
-	array1.insert(3);
-	array1.insert(4);
-
-	array2.insert(2);
-	array2.insert(3);
-	array2.insert(5);
+  //bool keepRunning = getContinue();
+  //cout << "Keep running is " << (keepRunning ? "true" : "false") << ".\n";
 
 
-	array1.display();
-	array2.display();
-	
-	unionTwoArrays(array1, array2, output);
+  Trie trie;
+
+  DynamicArray<int> path;
+  DynamicArray<DynamicArray<int>> allPaths;
+
+  trie.addNode(path, 5);
+  trie.addNode(path, 7);
+  trie.addNode(path, 10);
+
+  path.insert(5);
+  trie.addNode(path, 4);
+  trie.addNode(path, 3);
+  trie.addNode(path, 2);
+  trie.addNode(path, 1);
 
 
-	array1.display();
-	array2.display();
-	output.display();*/
+  trie.getAllPaths(allPaths);
 
-	
-	system("pause");
-	return 0;
+  for (int i = 0; i < allPaths.count(); i++)
+    allPaths[i].display();
+
+  /*
+  DynamicArray<int> array1, array2, output;
+
+  array1.insert(2);
+  array1.insert(3);
+  array1.insert(4);
+
+  array2.insert(2);
+  array2.insert(3);
+  array2.insert(5);
+
+
+  array1.display();
+  array2.display();
+
+  unionTwoArrays(array1, array2, output);
+
+
+  array1.display();
+  array2.display();
+  output.display();*/
+
+
+
+
+  system("pause");
+  return 0;
 }
