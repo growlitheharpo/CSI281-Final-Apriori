d4591bcbf49bbbc3e6fcdd27ff07899c84ff526a
James Keats <growlitheharpo@gmail.com>
2015-12-11 20:08:41 -0500

    Set up for actual test part 1

--- TeamWeiTooGood_FinalProject/inputoutput.h
+++ TeamWeiTooGood_FinalProject/inputoutput.h
@@ -4,13 +4,12 @@
 #include "basefunctions.h"
 #include "itemsetholder.h"
 
-const int NUM_OF_DATASETS = 5,
+const int NUM_OF_DATASETS = 27,
 		  NUM_OF_MINSUPPORTS = 3,
 		  NUM_OF_TRIALS = 3;
 
 const string DATASET_LIST[] =
 {
-	"T25.N0.1K.D10K.txt", //DELETE ME
 	"T5.N0.1K.D1K.txt",
 	"T5.N0.1K.D10K.txt",
 	"T5.N0.1K.D100K.txt",
